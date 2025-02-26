fd43bbf6cea32de67ea2d1e11841ea5b48b00162
AgFelix <marcelo12543@hotmail.com>
2025-02-26 10:33:42 -0300

    💄 style: Estilização CSS da img

--- src/components/AerisProfile.jsx
+++ src/components/AerisProfile.jsx
@@ -20,7 +20,6 @@ const AerisImageWrapper = styled.div`
 const AerisImg = styled.img`
   width: 100px; // Forçando o tamanho da imagem
   height: 100px; // Forçando o tamanho da imagem
-  object-fit: cover;
   border-radius: 50%; // Garantindo que a imagem seja redonda
 `;
 
