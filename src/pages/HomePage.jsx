cfa97f4d2ace2d341bf123e21fcb180703682fd7
AgFelix <marcelo12543@hotmail.com>
2025-02-26 11:17:52 -0300

    ✨ feat: adjust container text inicial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -93,7 +93,7 @@ const PresentationText = styled(motion.div)`
 `;
 
 export const HomePage = () => {
-  const presentationMessage = "Olá! Eu sou a Aires, sua tutora de ensino. Estou aqui para guiar você em sua jornada de aprendizado. Vamos começar?";
+  const presentationMessage = "Olá! Eu sou a Aeris, sua tutora de ensino. Estou aqui para guiar você em sua jornada de aprendizado. Vamos começar?";
 
   return (
     <>
