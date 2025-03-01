cf8c4ede0503bdd0730f809e4e7312a0bf2220ad
AgFelix <marcelo12543@hotmail.com>
2025-03-01 17:29:37 -0300

    ✨ feat: Add plugin vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'; // Importe o plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Aeris-Smart",
+        short_name: "Aeris",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/assets/Aeris.png", // Caminho relativo à raiz do projeto
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/assets/Aerus.png", // Caminho relativo à raiz do projeto
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 3000, // Porta do servidor de desenvolvimento
   },
   build: {
     outDir: 'dist', // Pasta de saída do build
-  },
+  }
 });
