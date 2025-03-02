9fd7e44881365a797646a3bd676e0bf7d3d7b9ac
AgFelix <marcelo12543@hotmail.com>
2025-03-02 00:28:43 -0300

    ♻️ refactor: containerinterface

--- src/components/InterfacePage.jsx
+++ src/components/InterfacePage.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  height: 91%;
   position: relative;
   overflow: hidden;
 
